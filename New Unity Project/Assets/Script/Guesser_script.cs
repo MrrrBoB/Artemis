546b7c54f68fd2404d243992a712f20c4ffc7985
Unknown <nathoward22@gmail.com>
2018-01-19 09:42:38 -0700

    Wrapped up basic code and scripts

--- New Unity Project/Assets/Script/Guesser_script.cs
+++ New Unity Project/Assets/Script/Guesser_script.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class Guesser_script : MonoBehaviour {
 
-	public int max = 1000;
+	public int max = 1001;
 	public int min = 1;
-	public int guess=650;
+	private int guess=650;
 
 	// Use this for initialization
 	private void Start () {
@@ -17,22 +17,37 @@ public class Guesser_script : MonoBehaviour {
 		print ("pick a number "+min+"-"+max);
 
 		// Ask if guess is within specified parameters
-		print("I guess "+guess);
-
+		NextGuess ();
 		//Instructions - if guess is higher than value, press up, if lower than value, press down.
 		print("Is your number higher? (up), lower (down) or same (enter)?");
+		max += 1;
 	}
-
+	private void NextGuess()
+	{
+		guess = (min + max) / 2;
+		print ("Is the number " + guess + "?");
+	}
 
 	// Update is called once per frame
 	public void Update () {
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) 
+		{
 			min = guess;
-			guess = (min + max) / 2;
-			print ("Is the number " + guess + "?");
+			NextGuess ();
 		}
-		if (Input.GetKeyDown (KeyCode.DownArrow)) {
-
+		else if (Input.GetKeyDown (KeyCode.DownArrow)) 
+		{
+			max = guess;
+			NextGuess ();
+		}
+		else if (Input.GetKeyDown (KeyCode.Return)) 
+		{
+			print ("That is correct!");
+			min = 0;
+			max = 1001;
+			print ("Pick another number");
+			guess = Random.Range (1, 1001);
+			print ("Is " + guess + " your number?");
 		}
 	}
 }
